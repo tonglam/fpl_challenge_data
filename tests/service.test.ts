698a0d121dc38e8f55fec07afe607d563b686697
tong <bluedragon00000@gmail.com>
2024-03-30 01:22:47 +0800

    minor changes.

--- tests/service.test.ts
+++
@@ -1,9 +0,0 @@
-const { fetchChallengeEvent } = require('../src/services/challege_event');
-const { describe, expect, it } = require('vitest');
-
-describe('fetchChallengeEvent', () => {
-  it('should fetch challenge event', async () => {
-    const data = await fetchChallengeEvent();
-    expect(data).toBeDefined();
-  });
-});
